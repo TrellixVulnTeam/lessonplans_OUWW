f9f055a3f940f1bedb69c9b629a787fb1668a815
Christopher Enoch <christopher.enoch@yahoo.co.uk>
2021-09-07 22:34:31 +0200

    Purchaseservice created

---
+++ src/main/java/com/enoch/chris/lessonplanwebsite/service/PurchaseService.java
@@ -0,0 +1,11 @@
+package com.enoch.chris.lessonplanwebsite.service;
+
+import java.util.List;
+
+import com.enoch.chris.lessonplanwebsite.entity.Purchase;
+
+public interface PurchaseService {
+
+	List<Purchase> findAllEager();
+
+}
--- src/test/java/com/enoch/chris/lessonplanwebsite/SubscriptionIntegrationTests.java
+++ src/test/java/com/enoch/chris/lessonplanwebsite/SubscriptionIntegrationTests.java
@@ -16,6 +16,8 @@ import com.enoch.chris.lessonplanwebsite.dao.UserRepository;
 import com.enoch.chris.lessonplanwebsite.entity.Subscription;
 import com.enoch.chris.lessonplanwebsite.entity.User;
 import com.enoch.chris.lessonplanwebsite.entity.utils.SubscriptionUtils;
+import com.enoch.chris.lessonplanwebsite.entity.utils.SubscriptionUtilsTest;
+import com.enoch.chris.lessonplanwebsite.service.PurchaseService;
 import com.enoch.chris.lessonplanwebsite.service.UsersService;
 
 @SpringBootTest
@@ -24,6 +26,9 @@ public class SubscriptionIntegrationTests {
 	private PurchaseRepository purchaseRepository;
 	private UserRepository userRepository;
 	private UsersService usersService;
+	
+	@Autowired
+	private PurchaseService purchaseService;
 
 	@Autowired
 	public SubscriptionIntegrationTests(PurchaseRepository purchaseRepository, UserRepository userRepository
@@ -35,7 +40,7 @@ public class SubscriptionIntegrationTests {
 	
 	@Test
 	public void shouldReturnNextStartDateNow(){
-//		User user = userRepository.findByUsername("lessonplantest");
+		User user = userRepository.findByUsername("lessonplantest");
 //		user.getBasket();
 //		user.getEmail();
 //		user.getEnabled();
@@ -48,7 +53,7 @@ public class SubscriptionIntegrationTests {
 		//User user = usersService.loadUserByUsername("lessonplantest");
 		//User user = userRepository.findById(28).get();
 		
-		User user = usersService.findUserByUsernameEager("lessonplantest");
+		//User user = usersService.findUserByUsernameEager("lessonplantest");
 		//Hibernate.initialize(user);
 		
 		//assertEquals("hello", "hello");
@@ -58,7 +63,40 @@ public class SubscriptionIntegrationTests {
 //		user.setUsername("lessonplantest");
 		
 		SubscriptionUtils subscriptionUtils = new SubscriptionUtils(new Subscription("A1"), user, purchaseRepository);
-		assertEquals(LocalDateTime.now(), subscriptionUtils.getNextSubscriptionStartDate());
+		assertEquals(LocalDateTime.now().getYear(), subscriptionUtils.getNextSubscriptionStartDate().getYear());
+//		
+//		assertEquals("Movies", categories.get(0).getCategory());
+//		assertEquals(1, categories.get(0).getId());
+//		assertEquals(2, categories.size());
+	}
+	
+	
+	@Test
+	public void shouldReturnNextStartDateNowTwo(){
+		User user = userRepository.findByUsername("lessonplantest");
+//		user.getBasket();
+//		user.getEmail();
+//		user.getEnabled();
+//		user.getiD();
+//		user.getPassword();
+//		user.getRoles();
+//		user.getSubscriptions();
+//		user.getUsername();
+		
+		//User user = usersService.loadUserByUsername("lessonplantest");
+		//User user = userRepository.findById(28).get();
+		
+		//User user = usersService.findUserByUsernameEager("lessonplantest");
+		//Hibernate.initialize(user);
+		
+		//assertEquals("hello", "hello");
+		
+//		User user = new User();
+//		user.setiD(28);
+//		user.setUsername("lessonplantest");
+		
+		SubscriptionUtilsTest subscriptionUtilsTest = new SubscriptionUtilsTest(new Subscription("A1"), user, purchaseService);
+		assertEquals(LocalDateTime.now().getYear(), subscriptionUtilsTest.getNextSubscriptionStartDate().getYear());
 //		
 //		assertEquals("Movies", categories.get(0).getCategory());
 //		assertEquals(1, categories.get(0).getId());
