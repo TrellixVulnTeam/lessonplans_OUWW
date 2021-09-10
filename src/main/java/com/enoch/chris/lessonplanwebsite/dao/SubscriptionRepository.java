9945c7b564ba871cd7f9a9f30a2540caecfbe080
Christopher Enoch <christopher.enoch@yahoo.co.uk>
2021-09-10 14:51:08 +0200

    Javadocs fro dao classes added

--- src/main/java/com/enoch/chris/lessonplanwebsite/dao/SubscriptionRepository.java
+++ src/main/java/com/enoch/chris/lessonplanwebsite/dao/SubscriptionRepository.java
@@ -19,10 +19,26 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Inte
 	
 	Optional<Subscription> findByName(String name);
 	
+	/**
+	 * Finds active subscriptions. An active subscription is deemed to be any subscription where 
+	 * dateSubscriptionEnds is after the current date. Time is taken into account. For instance a subscription with 
+	 * dateSubscriptionEnds equal to 15th March 2021 12:22 UTC will be deemed active at 12:20 UTC on 15th March 2021, but not at 12:23 UTC on this day.
+	 * @param user
+	 * @param date
+	 * @return
+	 */
 	@Query("SELECT s FROM Purchase as p inner join p.subscription as s where p.user = :user "
 			+ "and p.dateSubscriptionEnds > :date")
 	Set<Subscription> findActiveSubscriptions(@Param("user") User user, @Param("date") LocalDateTime date);
 	
+	/**
+	 * Finds active subscriptions ordered by name in ascending order. . An active subscription is deemed to be any subscription where 
+	 * dateSubscriptionEnds is after the current date. Time is taken into account. For instance a subscription with 
+	 * dateSubscriptionEnds equal to 15th March 2021 12:22 UTC will be deemed active at 12:20 UTC on 15th March 2021, but not at 12:23 UTC on this day.
+	 * @param user
+	 * @param date
+	 * @return
+	 */
 	@Query("SELECT s FROM Purchase as p inner join p.subscription as s where p.user = :user "
 			+ "and p.dateSubscriptionEnds > :date ORDER BY s.name")
 	LinkedHashSet<Subscription> findActiveSubscriptionsOrderByName(@Param("user") User user, @Param("date") LocalDateTime date);
